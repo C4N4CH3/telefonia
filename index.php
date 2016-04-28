e00b775748b08bfd374f1262ec4c11d5ebb63ace
canache <canache04@outlook.com>
2016-04-27 23:35:42 -0430

    Pagina web de empresa de telefonia

---
+++ index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Tel-Web</title>
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+
+	<!--Material Design Bootstrap
+	<link rel="stylesheet" type="text/css" href="css/material/material.min.css">
+	<link rel="stylesheet" type="text/css" href="css/material/ripples.min.css">
+	<link rel="stylesheet" type="text/css" href="css/material/roboto.min.css">
+	
+	<link rel="stylesheet" type="text/css" href="css/material/materialmodif.css">
+
+	<!-- Plugin CSS -->
+    <!--<link rel="stylesheet" href="css/theme/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/theme/creative.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" type="text/css" href="css/fonts/font1.css">
+	<link rel="stylesheet" type="text/css" href="css/fonts/font2.css">
+    <link rel="stylesheet" href="css/fonts/font-awesome.min.css" type="text/css">
+
+</head>
+<body>
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Tel-Web</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#planes">Planes</a>
+                    </li>
+                    <!--<li>
+                        <a class="page-scroll" href="#prepago">Prepago</a>
+                    </li>-->
+                    <li>
+                       <a class="page-scroll" href="#equipos">Equipos</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contacto">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1>Quieres rapidez <br> Quieres Tel-Web</h1>
+                <br>
+                <br>
+                <p>+1 millon de personas se unieron, solo faltas tu!</p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Iniciar Sesion</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="planes">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">PLANES ADAPTADOS PARA TI:</h2>
+
+
+                    <!--<hr class="light">-->      
+    				
+                    <!--<p class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
+                    <a href="#" class="btn btn-default btn-xl">Ingresa</a>-->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="prepago">
+        <!--<div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">PLANES ADAPTADOS PARA TI:</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>-->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-phone wow bounceIn text-primary"></i>
+                        <h3>De Voz</h3>
+                        <p class="text-muted">Comunicate con tus familiares y amigos al instante.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-globe wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                        <h3>Internacionales</h3>
+                        <p class="text-muted">¿Necesitas comunicarte al exterior? Cuenta con nosotros!.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <h3>Multimedia</h3>
+                        <p class="text-muted">We update dependencies to keep things fresh.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-comments-o wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                        <h3>De Datos</h3>
+                        <p class="text-muted">Si lo tuyo son las redes sociales tenemos lo mejor para ti!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="equipos">
+        <div class="container-fluid">
+            <div class="row no-gutter">
+                <div class="col-lg-4 col-sm-6"  align="center">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/portfolio/1.jpg" class="img-responsive" alt="" style="height: 242px;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    SAMSUNG
+                                </div>
+                                <div class="project-category text-faded">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6" align="center">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/portfolio/2.jpg" class="img-responsive" alt="" style="height: 242px;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    IPHONE
+                                </div>
+                                <div class="project-category text-faded">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6" align="center">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/portfolio/3.jpg" class="img-responsive" alt="" style="height: 242px;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    LG
+                                </div>
+                                <div class="project-category text-faded">
+                                    G3
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6" align="center">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/portfolio/4.jpg" class="img-responsive" alt="" style="height: 242px;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    BLU
+                                </div>
+                                <div class="project-category text-faded">
+                                    Life One, Studio 5c, Life Pure.
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6" align="center">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/portfolio/5.jpeg" class="img-responsive" alt="" style="height: 242px;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    HUAWEI
+                                </div>
+                                <div class="project-category text-faded">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6" align="center">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/portfolio/6.png" class="img-responsive" alt="" style="height: 242px;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    MOTOROLA
+                                </div><div class="project-category text-faded">
+                                    Moto G, Moto G2
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>¿Buscas otras marcas?</h2>
+                <a href="catalogo.php" class="btn btn-primary btn-xl wow tada">Catalogo</a>
+            </div>
+        </div>
+    </aside>
+
+    <section id="contacto">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                    <p>123-456-6789</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                    <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--Jquery-->
+<script type="text/javascript" src="js/jquery/jquery-1.12.3.min.js"></script>
+<!--Bootstrap-->
+<script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
+<!--Material Bootstrap
+<script type="text/javascript" src="js/material/material.min.js"></script>
+<script type="text/javascript" src="js/material/ripples.min.js"></script>
+<script>
+ $.material.init();
+</script>
+<!-- Plugin JavaScript -->
+    <script src="js/theme/jquery.easing.min.js"></script>
+    <script src="js/theme/jquery.fittext.js"></script>
+    <script src="js/theme/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/theme/creative.js"></script>
+
+</body>
+</html>
